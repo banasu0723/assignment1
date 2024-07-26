92ecdbd023faf87634fcfc19fa5843e41eec5ff9
banasu0723 <102410774+banasu0723@users.noreply.github.com>
2024-07-26 19:49:05 +0900

    init commit

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class App {
 
     public static void main(String[] args) {
+        ArrayList<Integer> intList = new ArrayList<Integer>();
         int[] intArray = new int[10];
         int cnt = 0;
         Scanner sc = new Scanner(System.in);
@@ -43,19 +44,25 @@ public class App {
                     }
                     break;
             }
-            intArray[cnt] = result;
+            intList.add(result);
+            //intArray[cnt] = result;
             cnt++;
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            more = sc.next();
+            if(more.equals("remove")){
+                intList.remove(0);
+            }
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            more = sc.next();
+            if(more.equals("inquiry")){
+                for(Integer e : intList){
+                    System.out.println(e);
+                }
+            }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             more = sc.next();
             if(more.equals("exit")){
                 break;
-            }else {
-                if (cnt == 10) {
-                    for (int i = 0; i < 9; i++) {
-                        intArray[i] = intArray[i + 1];
-                    }
-                    cnt = 9;
-                }
             }
         }
 
